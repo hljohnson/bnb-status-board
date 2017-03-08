02e7180931c1c36ac524a98768b754b1fb90fe13
Ashley Hutton <ashleyhutton119@gmail.com>
2017-03-08 11:17:51 -0600

    Changing puma config so hopefully this will work on heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,35 +1,15 @@
-if ENV.fetch("RAILS_ENV") == 'production'
-  # directory '/opt/iris'
-  # rackup "/opt/iris/config.ru"
-  # environment 'production'
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
-  # pidfile "/tmp/iris/puma.pid"
-  # state_path "/tmp/iris/puma.state"
-  # stdout_redirect '/var/log/iris/puma.access.log', '/var/log/iris/puma.error.log', true
+preload_app!
 
-  # threads 0,16
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
-  # bind 'unix:////tmp/iris/puma.sock'
-
-  # workers 0
-
-  # prune_bundler
-else
-  threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-  threads threads_count, threads_count
-  port        ENV.fetch("PORT") { 3000 }
-  environment ENV.fetch("RAILS_ENV") { "development" }
-  # The code in the `on_worker_boot` will be called if you are using
-  # clustered mode by specifying a number of `workers`. After each worker
-  # process is booted this block will be run, if you are using `preload_app!`
-  # option you will want to use this block to reconnect to any threads
-  # or connections that may have been created at application boot, Ruby
-  # cannot share connections between processes.
-  #
-  # on_worker_boot do
-  #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-  # end
-
-  # Allow puma to be restarted by `rails restart` command.
-  plugin :tmp_restart
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
