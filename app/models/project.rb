2e3072623fe03d48ffebb7b3f1a00c67f675086f
Ashley Hutton <ashleyhutton119@gmail.com>
2017-03-13 12:34:24 -0500

    Added states to projects (on track, at risk, etc)

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,34 @@ class Project < ActiveRecord::Base
 	# Relationships
 	has_many :tasks
 	belongs_to :user, class_name: "User", foreign_key: "owner_id"
-	#has_and_belongs_to_many :users
-	#has_many :tasks
-	# has_one :owner ???
+
+	include AASM
+	aasm do
+		state :on_track, :initial => true
+		state :at_risk
+		state :in_trouble
+		state :in_maintenance
+		state :waiting_on_client
+
+		event :set_on_track do
+			transitions :to => :on_track
+		end
+
+		event :set_at_risk do
+			transitions :to => :at_risk
+		end
+
+		event :set_in_trouble do
+			transitions :to => :in_trouble
+		end
+
+		event :set_in_maintenance do
+			transitions :to => :in_maintenance
+		end
+
+		event :set_waiting_on_client do
+			transitions :to => :waiting_on_client
+		end
+
+	end
 end
